61dfb84a3f766a1a9a4ebe5b8f2c3072e9d1b02b
MinhJD <145557059+JockDilson@users.noreply.github.com>
2024-09-29 12:06:09 +0700

    Create GCD_using_LCM.c
    find GCD based on the formular GDC(a,b) = a*b / LCM(a,b)

---
+++ ASSIGNMENT/Assignment_1/Solutions/NhatMinh_LCM/GCD_using_LCM.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int find_lcm(int a, int b) {
+    int larger = (a > b) ? a : b;
+    int lcm = larger;
+    while (lcm % a != 0 || lcm % b != 0) {
+        lcm += larger;
+    }
+    return lcm;
+}
+
+int find_gcd(int a, int b) {
+    int lcm = find_lcm(a, b);
+    return (a * b) / lcm;
+}
+
+int main() {
+    int a, b;
+    
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &a, &b);
+    
+    printf("GCD of %d and %d (using LCM) is: %d\n", a, b, find_gcd(a, b));
+    
+    return 0;
+}
